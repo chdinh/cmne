177b5c4fbff9b04f4c8cb10476a5efbc6d98941c
MNE <mne@localhost.localdomain>
2020-07-23 13:48:05 +0200

    uncomment continuous crossvalidation

--- I_implementation/I_cmne/I_hyperparameter_evaluation/eval_hyper.py
+++ I_implementation/I_cmne/I_hyperparameter_evaluation/eval_hyper.py
@@ -67,7 +67,7 @@ def eval_hyper(data_settings, data, training_settings, idx=None, idx_test=None):
                     # create the Data Generator
                     #data_generator = generate_lstm_future_batches(epochs=data.epochs(idx=idx), inverse_operator=data.inv_op(), lambda2=data.lambda2(), method=data.method(), look_back=lstm_look_back, future_steps=fs, batch_size=training_settings['minibatch_size'])
                     data_generator = generate_lstm_future_batches(epochs=data.train_epochs(idx=idx), inverse_operator=data.inv_op(), lambda2=data.lambda2(), method=data.method(), look_back=lstm_look_back, future_steps=fs, batch_size=training_settings['minibatch_size'])
-                    test_data_generator = generate_lstm_future_batches(epochs=data.train_epochs(idx=idx_test), inverse_operator=data.inv_op(), lambda2=data.lambda2(), method=data.method(), look_back=lstm_look_back, future_steps=fs, batch_size=training_settings['minibatch_size'], loop=False)
+                    #test_data_generator = generate_lstm_future_batches(epochs=data.train_epochs(idx=idx_test), inverse_operator=data.inv_op(), lambda2=data.lambda2(), method=data.method(), look_back=lstm_look_back, future_steps=fs, batch_size=training_settings['minibatch_size'], loop=False)
 
                     # create LSTM model
                     model = None
@@ -79,7 +79,8 @@ def eval_hyper(data_settings, data, training_settings, idx=None, idx_test=None):
                     model.compile(loss = 'mean_squared_error', optimizer = 'adam')
                 
                     # Train - fit the model :D
-                    fitting_result = model.fit_generator(data_generator, steps_per_epoch=training_settings['steps_per_ep'], epochs=training_settings['num_epochs'], verbose=1, validation_data=test_data_generator, validation_steps=num_tests, class_weight=None, workers=1, use_multiprocessing=False) #, callbacks=[tbCallBack])
+                    #fitting_result = model.fit_generator(data_generator, steps_per_epoch=training_settings['steps_per_ep'], epochs=training_settings['num_epochs'], verbose=1, validation_data=test_data_generator, validation_steps=num_tests, class_weight=None, workers=1, use_multiprocessing=False) #, callbacks=[tbCallBack])
+                    fitting_result = model.fit_generator(data_generator, steps_per_epoch=training_settings['steps_per_ep'], epochs=training_settings['num_epochs'], verbose=1, validation_data=None, class_weight=None, workers=1) #, callbacks=[tbCallBack])
                 
                     # # let's get some predictions
                     # test_predict = model.predict(test_features)
