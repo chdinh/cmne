17edf2cee0afb002f6cede0331fdee58d8959dbe
Christoph Dinh <christoph.dinh@mne-cpp.org>
2022-03-24 13:40:52 +0100

    fix projection

--- cmne/data.py
+++ cmne/data.py
@@ -171,7 +171,7 @@ class Data(object):
         include = []
         
         # set EEG average reference
-#        raw.set_eeg_reference()
+        raw.set_eeg_reference(copy=False, projection=True)
         
         # Add a bad channel
         #    raw.info['bads'] += ['EEG 053']  # bads + 1 more
